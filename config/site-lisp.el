d89f1e70f86ab0e6cadc06ceaef48824f2385af2
SHIOYA, Hiromu <kwappa.856@gmail.com>
2011-10-04 12:12:56 +0900

    install php-mode

--- config/site-lisp.el
+++ config/site-lisp.el
@@ -46,3 +46,8 @@
 
 ;; htmlize
 (require 'mediawiki)
+
+;; php-mode
+(autoload 'php-mode "php-mode" "Major mode for editing php code." t)
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc$" . php-mode))
